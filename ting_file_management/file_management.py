ef1fc4f5413db877a50de03af9760928ae79285e
brunogsr <brunogsr https://github.com/brunogsr>
2023-11-08 22:04:32 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.isfile(path_file):
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return None
+    if not path_file.endswith('.txt'):
+        sys.stderr.write('Formato inválido\n')
+        return None
+    with open(path_file, 'r') as file:
+        lines = file.read().split('\n')
+
+    return lines
