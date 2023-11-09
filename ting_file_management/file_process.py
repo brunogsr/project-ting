9576fd0d0cf723b08e5b7d8edd520e07f4d93ba7
brunogsr <brunogsr https://github.com/brunogsr>
2023-11-08 23:32:53 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    try:
+        processed_files = {item["nome_do_arquivo"] for item in instance.queue}
+        if path_file not in processed_files:
+            lines = txt_importer(path_file)
+            if lines is not None:
+                file_data = {
+                    "nome_do_arquivo": path_file,
+                    "qtd_linhas": len(lines),
+                    "linhas_do_arquivo": lines,
+                }
+                instance.enqueue(file_data)
+                print(file_data)
+    except Exception as e:
+        print(f"Erro ao processar o arquivo: {e}")
 
 
 def remove(instance):
