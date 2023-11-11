2964b883b24cc06769286df11bb95050eb7b59da
brunogsr <brunogsr https://github.com/brunogsr>
2023-11-11 19:01:24 -0300

    req 7 e 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,44 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for index in range(len(instance)):
+        file_data = instance.search(index)
+        filename = file_data["nome_do_arquivo"]
+        lines = file_data["linhas_do_arquivo"]
+
+        occurrences = [
+            {"linha": line_num + 1}
+            for line_num, line in enumerate(lines)
+            if word.lower() in line.lower()
+        ]
+
+        if occurrences:
+            results.append({
+                "palavra": word,
+                "arquivo": filename,
+                "ocorrencias": occurrences
+            })
+
+    return results
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for index in range(len(instance)):
+        file_data = instance.search(index)
+        filename = file_data["nome_do_arquivo"]
+        lines = file_data["linhas_do_arquivo"]
+
+        occurrences = [
+            {"linha": line_num + 1, "conteudo": line}
+            for line_num, line in enumerate(lines)
+            if word.lower() in line.lower()]
+
+        if occurrences:
+            result = {"arquivo": filename, "ocorrencias": occurrences}
+            if word.lower() in result["ocorrencias"][0]["conteudo"].lower():
+                result["palavra"] = word
+            results.append(result)
+
+    return results
